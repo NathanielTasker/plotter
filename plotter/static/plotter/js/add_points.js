eb878c3be9745923167d923f315ed062e1ab2ed2
NathanielTasker <natsumit.fi@gmail.com>
2017-12-29 00:09:38 +0900

    removed test codes from add_points.js, index.html

--- plotter/static/plotter/js/add_points.js
+++ plotter/static/plotter/js/add_points.js
@@ -1,7 +1,6 @@
 var points = 0;
 
 document.getElementById( "plot_area" ).addEventListener( "click", function( event ) {
-    console.log('hi'); //just for test
     var clickX = event.pageX ;
     var clickY = event.pageY ;
 
@@ -12,12 +11,11 @@ document.getElementById( "plot_area" ).addEventListener( "click", function( even
     var x = clickX - positionX ;
     var y = clickY - positionY ;
 
-    // $('plot_area').append($("<div class='point_form'><p><label for='point_" + points + "'><input type='text' name='point_name_" + points + "' /></label></p><div class='color picker'>color picker</div><p><input type='text' name='point_x_" + points + "' value=" + x + " /></p><p><input type='text' name='point_y_" + points + "' value=" + y + " /></p></div>").css({
-    $('plot_area').append($("<p>add point here!</p>").css({
+    $('body').append($("<div class='point_form'><p><label for='point_" + points + "'><input type='text' name='point_name_" + points + "' /></label></p><div class='color picker'>color picker</div><p><input type='text' name='point_x_" + points + "' value=" + x + " /></p><p><input type='text' name='point_y_" + points + "' value=" + y + " /></p></div>").css({
         position: 'absolute',
         left: clickX,
         top:  clickY,
-        backgroundColor: "#aaaaaa"
+        backgroundColor: "#eeeeee"
     }));
 
     points ++;
