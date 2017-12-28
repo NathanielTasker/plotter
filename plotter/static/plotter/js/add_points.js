f546df757b5feb2162456f116a87d2600664e8e9
NathanielTasker <natsumit.fi@gmail.com>
2017-12-29 00:34:23 +0900

    made x, y invisible from point adding form

--- plotter/static/plotter/js/add_points.js
+++ plotter/static/plotter/js/add_points.js
@@ -13,7 +13,7 @@ document.getElementById( "plot_area" ).addEventListener( "click", function( even
     var x = clickX - positionX ;
     var y = clickY - positionY ;
 
-    $('#plot_area').append($("<div class='point_form'><p><label for='point_" + points + "'><input type='text' name='point_name_" + points + "' /></label></p><div class='color picker'>color picker</div><p><input type='text' name='point_x_" + points + "' value=" + x + " /></p><p><input type='text' name='point_y_" + points + "' value=" + y + " /></p></div>").css({
+    $('#plot_area').append($("<div class='point_form'><p><label for='point_" + points + "'><input type='text' name='point_name_" + points + "' /></label></p><div class='color picker'>color picker</div><p><input type='text' name='point_x_" + points + "' value=" + x + " class='display_none' /></p><p><input type='text' name='point_y_" + points + "' value=" + y + " class='display_none' /></p></div>").css({
         position: 'absolute',
         left: clickX,
         top:  clickY,
