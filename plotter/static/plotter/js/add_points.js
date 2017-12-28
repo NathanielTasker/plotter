093ba6234602d5b0dfa958252ea547c4f1b9063b
NathanielTasker <natsumit.fi@gmail.com>
2017-12-29 00:38:10 +0900

    made html codes in add_points.js more simple upnvi# Changes to be committed:

--- plotter/static/plotter/js/add_points.js
+++ plotter/static/plotter/js/add_points.js
@@ -13,11 +13,10 @@ document.getElementById( "plot_area" ).addEventListener( "click", function( even
     var x = clickX - positionX ;
     var y = clickY - positionY ;
 
-    $('#plot_area').append($("<div class='point_form'><p><label for='point_" + points + "'><input type='text' name='point_name_" + points + "' /></label></p><div class='color picker'>color picker</div><p><input type='text' name='point_x_" + points + "' value=" + x + " class='display_none' /></p><p><input type='text' name='point_y_" + points + "' value=" + y + " class='display_none' /></p></div>").css({
+    $('#plot_area').append($("<div class='point_form'><p><label for='point_" + points + "'><input type='text' name='point_name_" + points + "' /></label></p><input type='text' name='point_x_" + points + "' value=" + x + " class='display_none' /><input type='text' name='point_y_" + points + "' value=" + y + " class='display_none' /></div>").css({
         position: 'absolute',
         left: clickX,
         top:  clickY,
-        backgroundColor: "#eeeeee"
     }));
 
     console.log(points); //just for test
