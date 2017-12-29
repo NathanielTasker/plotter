57802f4a82a7538aa30d7c9cd45a33665bb963a7
NathanielTasker <natsumit.fi@gmail.com>
2017-12-29 19:59:03 +0900

    added focusing new point form when it appears to add_points.js

--- plotter/static/plotter/js/add_points.js
+++ plotter/static/plotter/js/add_points.js
@@ -13,11 +13,14 @@ document.getElementById( "plot_area" ).addEventListener( "click", function( even
     var x = clickX - positionX ;
     var y = clickY - positionY ;
 
-    $('#plot_area').append($("<div class='point_form'><p><label for='point_" + points + "'><input type='text' name='point_name_" + points + "' /></label></p><input type='text' name='point_x_" + points + "' value=" + x + " class='display_none' /><input type='text' name='point_y_" + points + "' value=" + y + " class='display_none' /></div>").css({
+    $('#plot_area').append($("<div class='point_form'><p><label for='point_" + points + "'><input type='text' name='point_name_" + points + "' id='point_name_" + points + "' /></label></p><input type='text' name='point_x_" + points + "' value=" + x + " class='display_none' /><input type='text' name='point_y_" + points + "' value=" + y + " class='display_none' /></div>").css({
         position: 'absolute',
         left: clickX,
         top:  clickY,
     }));
+
+    var focusElm = document.getElementById("point_name_" + points);
+    focusElm.focus();
 
     console.log(points); //just for test
     return points;
