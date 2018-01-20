4e3019ac9ac4e481a4f0ce0d64ddc55cb09929b1
NathanielTasker <natsumit.fi@gmail.com>
2018-01-20 14:51:05 +0900

    prepared for showing points in results view

---
+++ plotter/static/plotter/js/show_points.js
@@ -0,0 +1,11 @@
+var point = ;
+
+var point_name = ;
+var x =  ;
+var y =  ;
+
+$('#plot_area').append($("<p>" + point_name + "</p>").css({
+    position: 'absolute',
+    left: x,
+    top:  y,
+}));
